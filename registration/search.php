ef38ab081b92f22a4a4dae6ac807b631880ba629
Alex Emms <me@alexemms.com>
2013-10-31 15:49:07 +0000

    Change search

--- registration/search.php
+++ registration/search.php
@@ -56,5 +56,47 @@
      echo "<b>Searched For:</b> " .$find; 
      } 
      ?> 
+</body><h1>Search here!</h1>
+<p>Fill in the name to search then click <strong>Submit</strong> to search.</p>
+<form name="search" method="post" action="search.php">
+    Search:<input type="text" name="query" value=""/>
+    <input type="submit" name="completedSearch" value="Find" />
+</form>
+<?php
+    $host = "eu-cdbr-azure-west-b.cloudapp.net";
+    $user = "b4d71357152f0e";
+    $pwd = "ed689257";
+    $db = "systemsA12lVzdp3";
+    if(isset($_POST['completedSearch'])) {
+        $term = $_POST['query'];
+        
+        mysql_connect($host, $user, $pwd);
+        mysql_select_db($db);
+
+        $qu = mysql_query("SELECT * FROM registration_tbl WHERE name LIKE '%{$term}%' OR email LIKE '%{$term}%' OR companyName LIKE '%{$term}%';");
+
+        echo "<table>
+              <th>Name Results</th>
+              <th>Email Results</th>
+              <th>Company Name Results</th>
+              <br>
+             ";
+
+        while($row = mysql_fetch_array($qu)) {
+            echo "<tr><td>";  
+            echo $row['name'];
+            echo " </td>";
+            echo "<td>";
+            echo $row['email'];
+            echo " </td>";
+            echo "<td>";
+            echo $row['companyName'];
+            echo "<br>";
+            echo " </td></tr>";
+        }
+        echo "</table>";
+        echo "HELLO!";
+    }
+?>
 </body>
 <?php require_once("footer.php") ?>
